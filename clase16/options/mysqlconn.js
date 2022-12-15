18f845f619bd4a5c46885b4b46e267abfdd42b2c
Agustin <aguspaparo@gmail.com>
2022-12-14 23:50:50 -0300

    Nuestra primer base de datos

---
+++ clase16/options/mysqlconn.js
@@ -0,0 +1,10 @@
+const conectionProducts = {
+    client: "mysql",
+    connection:{
+        host: "localhost",
+        user: "root",
+        password: "root1",
+        database: "ecommerce"
+    }
+}
+module.exports = {conectionProducts}
